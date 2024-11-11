3ede55d8fba7cc0075b7ee11c3641af91926aa66
Apple WW <developerapplewarnawarni@gmail.com>
2024-11-11 10:25:40 +0700

    Initial Commit

---
+++ UIKitCoordinatorTests/UIKitCoordinatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitCoordinatorTests.swift
+//  UIKitCoordinatorTests
+//
+//  Created by MacBook Air MII  on 11/11/24.
+//
+
+import Testing
+@testable import UIKitCoordinator
+
+struct UIKitCoordinatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
